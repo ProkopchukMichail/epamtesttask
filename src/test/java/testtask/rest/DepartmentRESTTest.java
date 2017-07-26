4184bd5384ddb704d1c9ee9ade1db6ffb8ad0c1b
comp <prokopchukmichail@gmail.com>
2017-07-26 19:22:15 +0300

    rest full implement without rest test2

--- src/test/java/testtask/rest/DepartmentRESTTest.java
+++ src/test/java/testtask/rest/DepartmentRESTTest.java
@@ -1,7 +1,15 @@
 package testtask.rest;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by comp on 26.07.2017.
  */
+@ContextConfiguration("classpath:spring/spring-db.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("hsql")
 public class DepartmentRESTTest {
 }
